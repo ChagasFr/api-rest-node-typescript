d726134b11ab4cdc6772a8fa36c309be66f5c7b6
Felipe chagas <felipereis9998@aluno.saojudas.br>
2025-03-26 19:19:43 -0300

    feat: criando validation

--- src/server/shared/middleware/Validation.ts
+++ src/server/shared/middleware/Validation.ts
@@ -1,7 +1,22 @@
 import { RequestHandler } from "express";
+import { StatusCodes } from "http-status-codes";
+import { ObjectSchema, ValidationError } from "yup";
 
-type TValidation = () => RequestHandler;
+type TValidation = (scheme: ObjectSchema<any>) => RequestHandler;
 
-export const validation = () => async (req, res, next) => {
-  console.log("test");
+export const validation: TValidation = (scheme) => async (req, res, next) => {
+  try {
+    await scheme.validate(req.body);
+  } catch (err) {
+    const yupError = err as ValidationError;
+    const errors: Record<string, string> = {};
+
+    yupError.inner.forEach((error) => {
+      if (!error.path) return;
+
+      errors[error.path] = error.message;
+    });
+
+    return res.status(StatusCodes.BAD_REQUEST).json({ errors });
+  }
 };
