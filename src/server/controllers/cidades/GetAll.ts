ca36eaf9e4fa2b8a8c368cabcb0c945713ed1b3a
Felipe chagas <felipereis9998@aluno.saojudas.br>
2025-04-01 18:32:13 -0300

    fix: ajuste get all

--- src/server/controllers/cidades/GetAll.ts
+++ src/server/controllers/cidades/GetAll.ts
@@ -1,5 +1,7 @@
-import { Request, RequestHandler, Response } from "express";
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 import * as yup from "yup";
+
 import { validation } from "../../shared/middleware";
 
 interface IQueryProps {
@@ -7,7 +9,6 @@ interface IQueryProps {
   limit?: number;
   filter?: string;
 }
-
 export const getAllValidation = validation((getSchema) => ({
   query: getSchema<IQueryProps>(
     yup.object().shape({
@@ -18,9 +19,13 @@ export const getAllValidation = validation((getSchema) => ({
   ),
 }));
 
-export const getAll: RequestHandler = async (
-  _req: Request<{}, {}, {}, IQueryProps>,
+export const getAll = async (
+  req: Request<{}, {}, {}, IQueryProps>,
   res: Response
 ) => {
-  return res.send("Create");
+  console.log(req.query);
+
+  return res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .send("Não implementado!");
 };
