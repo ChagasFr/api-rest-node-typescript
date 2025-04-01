985af119c8ee2b1d02ed3a2cd1194e8afde6411d
Felipe chagas <felipereis9998@aluno.saojudas.br>
2025-04-01 18:48:20 -0300

    fix: ajuste get all

--- src/server/controllers/cidades/DeleteById.ts
+++ src/server/controllers/cidades/DeleteById.ts
@@ -4,26 +4,19 @@ import * as yup from "yup";
 
 import { validation } from "../../shared/middleware";
 
-interface IQueryProps {
-  page?: number;
-  limit?: number;
-  filter?: string;
+interface IParamProps {
+  id?: number;
 }
-export const getAllValidation = validation((getSchema) => ({
-  query: getSchema<IQueryProps>(
+export const deleteByIdValidation = validation((getSchema) => ({
+  params: getSchema<IParamProps>(
     yup.object().shape({
-      page: yup.number().notRequired().moreThan(0),
-      limit: yup.number().notRequired().moreThan(0),
-      filter: yup.string().notRequired(),
+      id: yup.number().integer().required().moreThan(0),
     })
   ),
 }));
 
-export const getAll = async (
-  req: Request<{}, {}, {}, IQueryProps>,
-  res: Response
-) => {
-  console.log(req.query);
+export const deleteById = async (req: Request<IParamProps>, res: Response) => {
+  console.log(req.params);
 
   return res
     .status(StatusCodes.INTERNAL_SERVER_ERROR)
