dd063324d2d7eb349dea70c08a54b9fb007b2260
Felipe chagas <felipereis9998@aluno.saojudas.br>
2025-04-07 23:46:02 -0300

    feat: create describe

--- tests/cidades/Create.test.ts
+++ tests/cidades/Create.test.ts
@@ -1,1 +1,7 @@
-describe("Cidades - Create", () => {});
+import { testServer } from "../jest.setup";
+
+describe("Cidades - Create", () => {
+  it("Cria registro", () => {
+    testServer.post("/cidades").send({ nome: "Caxias do Sul" });
+  });
+});
