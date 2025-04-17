c35373bd5533ab32a11b51eb2df30dc509efb741
Felipe chagas <felipereis9998@aluno.saojudas.br>
2025-04-17 00:32:05 -0300

    feat: ambientes da aplicacaos

--- src/server/database/migrations/0000_create_cidade.ts
+++ src/server/database/migrations/0000_create_cidade.ts
@@ -1,8 +1,14 @@
 import { Knex } from "knex";
+import { ETableNames } from "../ETableNames";
 
-export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable("cidade", (table) => {
-    table.bigIncrements();
+export async function up(knex: Knex) {
+  return knex.schema.createTable(ETableNames.cidade, (table) => {
+    table.bigIncrements("id").primary().index();
+    table.string("nome", 150).index().notNullable();
+
+    table.comment("Tabela poara armazenar as cidades do sistema.");
   });
 }
-export async function down(knex: Knex): Promise<void> {}
+export async function down(knex: Knex) {
+  return knex.schema.dropTable(ETableNames.cidade);
+}
