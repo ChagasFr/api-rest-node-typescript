c5781adc87180a21b01a42f9aa9c8f5d6cae88dd
Felipe chagas <felipereis9998@aluno.saojudas.br>
2025-05-21 01:07:04 -0300

    fix: ajuste teste singup

--- tests/usuarios/SingUp.test.ts
+++ tests/usuarios/SingUp.test.ts
@@ -10,7 +10,7 @@ describe("Usuario - SingUp", () => {
     cidadeId = resCidade.body;
   });
 
-  it("Cadastra usuario1", async () => {
+  it("Cadastra usuario 1", async () => {
     const res1 = await testServer.post("/cadastrar").send({
       senha: "123456",
       nome: "Juca da Silva",
@@ -31,68 +31,71 @@ describe("Usuario - SingUp", () => {
     expect(typeof res1.body).toEqual("number");
   });
   it("Erro ao cadastrar um usuario com email duplicado", async () => {
-    const res1 = await testServer.post("/pessoas").send({
-      cidadeId,
-      nomeCompleto: "Juca silva",
-      email: "jucaduplicado@gmail.com",
+    const res1 = await testServer.post("/cadastrar").send({
+      senha: "123456",
+      nome: "Pedro da Rosa",
+      email: "pedroduplicado@gmail.com",
     });
     expect(res1.statusCode).toEqual(StatusCodes.CREATED);
     expect(typeof res1.body).toEqual("number");
 
-    const res2 = await testServer.post("/pessoas").send({
-      cidadeId,
-      email: "jucaduplicado@gmail.com",
-      nomeCompleto: "duplicado",
+    const res2 = await testServer.post("/cadastrar").send({
+      senha: "123456",
+      nome: "Juca da silva",
+      email: "pedroduplicado@gmail.com",
     });
     expect(res2.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
     expect(res2.body).toHaveProperty("errors.default");
   });
-  it("Tenta criar registro com nomeCompleto muito curto", async () => {
-    const res1 = await testServer.post("/pessoas").send({
-      cidadeId,
-      email: "juca@gmail.com",
-      nomeCompleto: "Ju",
-    });
-
-    expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
-    expect(res1.body).toHaveProperty("errors.body.nomeCompleto");
-  });
-  it("Tenta criar registro sem nomeCompleto", async () => {
-    const res1 = await testServer.post("/pessoas").send({
-      cidadeId,
-      email: "juca@gmail.com",
-    });
-
-    expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
-    expect(res1.body).toHaveProperty("errors.body.nomeCompleto");
-  });
-  it("Tenta criar registro sem email", async () => {
-    const res1 = await testServer.post("/pessoas").send({
-      cidadeId,
-      nomeCompleto: "Juca da Silva",
+  it("Erro ao cadastrar um usuario sem email", async () => {
+    const res1 = await testServer.post("/cadastrar").send({
+      senha: "123456",
+      nome: "Pedro da Rosa",
+      // email: "pedrodarosa@gmail.com",
     });
 
     expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
     expect(res1.body).toHaveProperty("errors.body.email");
   });
-  it("Tenta criar registro com email inválido", async () => {
-    const res1 = await testServer.post("/pessoas").send({
-      cidadeId,
-      email: "juca gmail.com",
-      nomeCompleto: "Juca da Silva",
+  it("Erro ao cadastrar um usuario sem nome", async () => {
+    const res1 = await testServer.post("/cadastrar").send({
+      senha: "123456",
+      // nome: "Pedro da Rosa",
+      email: "pedrodarosa@gmail.com",
+    });
+
+    expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+    expect(res1.body).toHaveProperty("errors.body.nome");
+  });
+  it("Erro ao cadastrar um usuario sem senha", async () => {
+    const res1 = await testServer.post("/cadastrar").send({
+      // senha: "123456",
+      nome: "Pedro da Rosa",
+      email: "pedrodarosa@gmail.com",
+    });
+
+    expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
+    expect(res1.body).toHaveProperty("errors.body.senha");
+  });
+  it("Erro ao cadastrar um usuario com email inválido", async () => {
+    const res1 = await testServer.post("/cadastrar").send({
+      senha: "123456",
+      nome: "Pedro da Rosa",
+      email: "pedrodarosa gmail.com",
     });
 
     expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
     expect(res1.body).toHaveProperty("errors.body.email");
   });
-  it("Tenta criar registro sem cidadeId", async () => {
-    const res1 = await testServer.post("/pessoas").send({
-      email: "juca@gmail.com",
-      nomeCompleto: "Juca da Silva",
+  it("Erro ao cadastrar um usuario com senha muito pequena", async () => {
+    const res1 = await testServer.post("/cadastrar").send({
+      senha: "123",
+      nome: "Juca da silva",
+      email: "jucasilva@gmail.com",
     });
 
     expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
-    expect(res1.body).toHaveProperty("errors.body.cidadeId");
+    expect(res1.body).toHaveProperty("errors.body.senha");
   });
   it("Tenta criar registro com cidadeId inválido", async () => {
     const res1 = await testServer.post("/pessoas").send({
