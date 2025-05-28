6f629ec1a133f756507e5722182298a2045d047a
Felipe chagas <felipereis9998@aluno.saojudas.br>
2025-05-28 17:47:02 -0300

    feat: service

--- src/server/shared/services/JWTService.ts
+++ src/server/shared/services/JWTService.ts
@@ -4,11 +4,28 @@ interface IJwtData {
   uid: number;
 }
 
-const sign = (data: IJwtData) => {
-  return jwt.sign(data, "23711982437198372");
+const sign = (data: IJwtData): string | "JWT_SECRET_NOT_FOUND" => {
+  if (!process.env.JWT_SECRET) return "JWT_SECRET_NOT_FOUND";
+
+  return jwt.sign(data, process.env.JWT_SECRET, { expiresIn: "24h" });
 };
 
-const verify = () => {};
+const verify = (
+  token: string
+): IJwtData | "JWT_SECRET_NOT_FOUND" | "INVALID_TOKEN" => {
+  if (!process.env.JWT_SECRET) return "JWT_SECRET_NOT_FOUND";
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (typeof decoded === "string") {
+      return "INVALID_TOKEN";
+    }
+
+    return decoded as IJwtData;
+  } catch (error) {
+    return "INVALID_TOKEN";
+  }
+};
 
 export const JWTService = {
   sign,
