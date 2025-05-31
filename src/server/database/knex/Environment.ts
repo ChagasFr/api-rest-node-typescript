085e9be38e11d5f5d82d46a41233819f49d37885
ChagasFr <felipechagasdosreis@gmail.com>
2025-05-31 14:07:18 -0300

    feat: environment

--- src/server/database/knex/Environment.ts
+++ src/server/database/knex/Environment.ts
@@ -1,31 +1,46 @@
-import { Knex } from "knex";
-import path from "path";
+import { Knex } from 'knex';
+import path from 'path';
+
 
 export const development: Knex.Config = {
-  client: "sqlite3",
+  client: 'sqlite3',
   useNullAsDefault: true,
   connection: {
-    filename: path.resolve(__dirname, "..", "..", "..", "db.sqlite"),
+    filename: path.resolve(__dirname, '..', '..', '..', '..', 'database.sqlite')
   },
   migrations: {
-    directory: path.resolve(__dirname, "..", "migrations"),
+    directory: path.resolve(__dirname, '..', 'migrations'),
   },
   seeds: {
-    directory: path.resolve(__dirname, "..", "seeds"),
+    directory: path.resolve(__dirname, '..', 'seeds'),
   },
   pool: {
     afterCreate: (connection: any, done: Function) => {
-      connection.run("PRAGMA foreign_keys = ON");
+      connection.run('PRAGMA foreign_keys = ON');
       done();
-    },
-  },
+    }
+  }
 };
 
 export const test: Knex.Config = {
   ...development,
-  connection: ":memory:",
+  connection: ':memory:',
 };
 
 export const production: Knex.Config = {
-  ...development,
+  client: 'pg',
+  migrations: {
+    directory: path.resolve(__dirname, '..', 'migrations'),
+  },
+  seeds: {
+    directory: path.resolve(__dirname, '..', 'seeds'),
+  },
+  connection: {
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: Number(process.env.DATABASE_PORT || 5432),
+    ssl: { rejectUnauthorized: false },
+  },
 };
